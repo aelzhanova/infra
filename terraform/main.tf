120b756f83779659d7d49c319d7176047aa397d9
aelzhanova <alsu.elzhanova@gmail.com>
2017-10-14 19:34:54 +0300

    homework #9

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,60 +2,13 @@ provider "google" {
   project = "${var.project}"
   region  = "${var.region}"
 }
-
-resource "google_compute_instance" "app" {
-  name         = "reddit-app"
-  machine_type = "g1-small"
-  zone         = "europe-west1-b"
-
-  # определение загрузочного диска
-  boot_disk {
-    initialize_params {
-      image = "${var.disk_image}"
-    }
-  }
-
-  metadata {
-    sshKeys = "appuser:${file(var.public_key_path)}"
-  }
-
-  tags = ["reddit-app"]
-
-  # определение сетевого интерфейса
-  network_interface {
-    # сеть, к которой присоединить данный интерфейс
-    network = "default"
-
-    # использовать ephemeral IP для доступа из Интернет
-    access_config {}
-  }
-
-  connection {
-    type        = "ssh"
-    user        = "appuser"
-    agent       = false
-    private_key = "${file(var.private_key_path)}"
-  }
-
-  provisioner "file" {
-    source      = "files/puma.service"
-    destination = "/tmp/puma.service"
-  }
-
-  provisioner "remote-exec" {
-    script = "files/deploy.sh"
-  }
+module "app" {
+source = "modules/app"
+public_key_path = "${var.public_key_path}"
+app_disk_image = "${var.app_disk_image}"
 }
-
-resource "google_compute_firewall" "firewall_puma" {
-  name    = "allow-puma-default"
-  network = "default"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["9292"]
-  }
-
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["reddit-app"]
+module "db"{
+source = "modules/db"
+public_key_path = "${var.public_key_path}"
+app_disk_image = "${var.db_disk_image}"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,5 @@
 variable project {
-  description = "infra-179515"
+  description = "Project ID"
 }
 
 variable region {
@@ -8,13 +8,21 @@ variable region {
 }
 
 variable public_key_path {
-  description = "C:/Users/alsug/.ssh/appuser.pub"
+  description = "Path to the public key used to connect to instance"
 }
 
 variable disk_image {
-  description = "reddit-base-1505060019"
+  description = "Disk image"
 }
 
 variable private_key_path {
-  description = "C:/Users/alsug/.ssh/appuser"
+  description = "Path to the private key"
+}
+variable app_disk_image {
+	description = "Disk image for reddit app"
+	default = "reddit-app-base"
+}
+variable db_disk_image {
+description = "Disk image for reddit db"
+default = "reddit-db-base"
 }
