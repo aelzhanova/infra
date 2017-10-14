00932288d85114ce33ecce2cabd6a2bb95f74917
aelzhanova <alsu.elzhanova@gmail.com>
2017-10-14 16:51:37 +0300

    homework #8. Work with variables in the Terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,20 @@
+variable project {
+  description = "infra-179515"
+}
+
+variable region {
+  description = "Region"
+  default     = "europe-west1"
+}
+
+variable public_key_path {
+  description = "C:/Users/alsug/.ssh/appuser.pub"
+}
+
+variable disk_image {
+  description = "reddit-base-1505060019"
+}
+
+variable private_key_path {
+  description = "C:/Users/alsug/.ssh/appuser"
+}
